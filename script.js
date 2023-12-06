919f1dfecf32f7d671a3fff12a64074682f83c45
vladh <hadyak305246@gmail.com>
2023-12-05 22:09:17 -0700

    Create basic UI for Book Library

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 const libraryContainer = document.querySelector(".library-wrapper");
+const modalBtn = document.querySelector(".modal-btn");
 
 const myLibrary = [];
 
@@ -7,39 +8,33 @@ function Book(title) {
   this.title = title;
 };
 
- // User input
+ // User inputs 
 const book1 = new Book("Book1");         
 const book2 = new Book("Book2");
+const book3 = new Book("Book3");
 
 function addBookToLibrary() {
   myLibrary.push(book1);
   myLibrary.push(book2);
+  myLibrary.push(book3);
 };
 addBookToLibrary();
 
-function displayBooks() {
+function displayBook() {
   myLibrary.forEach((book) => {
-    console.log(book);
+    let bookCard = document.createElement("div");
+    bookCard.classList.add("book-sample");
+    libraryContainer.appendChild(bookCard);
+
+
+    let bookTitle = document.createElement("h1");
+    bookCard.appendChild(bookTitle);
+
+    let bookImg = document.createElement("img");
+    bookImg.classList.add("book-cover");                  // Generate random cover for each new book created ****
+    bookCard.appendChild(bookImg); 
+
+    bookTitle.textContent = book.title;
   });
 };
-
-displayBooks();
-
-
-
-
-
-
-
-
-
-
-
-//for (let obj of myLibrary) {
-  //for (let key in obj) {
-    //console.log("-Book Obj-");
-    //if (obj.hasOwnProperty(key)) {
-      //console.log(`${key}: ${obj[key]}`)
-    //};
-  //};
-//};
+displayBook();
